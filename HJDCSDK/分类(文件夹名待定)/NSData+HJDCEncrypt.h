b19911c86c8081056abbd3bfe89512817e865443
WangAn <>
2018-05-29 15:00:36 +0800

    new file:   "HJDCSDK/\345\210\206\347\261\273(\346\226\207\344\273\266\345\244\271\345\220\215\345\276\205\345\256\232)/NSData+HJDCEncrypt.h" 	new file:   "HJDCSDK/\345\210\206\347\261\273(\346\226\207\344\273\266\345\244\271\345\220\215\345\276\205\345\256\232)/NSData+HJDCEncrypt.m"

---
+++ HJDCSDK/分类(文件夹名待定)/NSData+HJDCEncrypt.h
@@ -0,0 +1,12 @@
+//
+//  NSData+HJDCEncrypt.h
+//  HJDCSDK
+//
+//  Created by WangAn on 2018/5/29.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (HJDCEncrypt)
+
+@end
---
+++ HJDCSDK/分类(文件夹名待定)/NSData+HJDCEncrypt.m
@@ -0,0 +1,12 @@
+//
+//  NSData+HJDCEncrypt.m
+//  HJDCSDK
+//
+//  Created by WangAn on 2018/5/29.
+//
+
+#import "NSData+HJDCEncrypt.h"
+
+@implementation NSData (HJDCEncrypt)
+
+@end
