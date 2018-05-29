28e735d3d517726f708aae780532efd3f750aa7e
FuYunLei <1053591151@qq.com>
2018-05-29 14:34:51 +0800

    添加NSString 分类

---
+++ HJDCSDK/分类(文件夹名待定)/NSString+HJDC.h
@@ -0,0 +1,33 @@
+//
+//  NSString+HJDC.h
+//  HJDC
+//
+//  Created by FuYunLei on 2018/5/29.
+//  Copyright © 2018年 FuYunLei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (HJDC)
+
+/**
+ 将当前字符串拼接到cache目录后面
+
+ @return path
+ */
+- (NSString *)hjdc_cacheDir;
+
+/**
+ 将当前字符串拼接到doc目录后面
+ 
+ @return path
+ */
+- (NSString *)hjdc_docDir;
+/**
+ 将当前字符串拼接到temp目录后面
+ 
+ @return path
+ */
+- (NSString *)hjdc_tempDir;
+
+@end
---
+++ HJDCSDK/分类(文件夹名待定)/NSString+HJDC.m
@@ -0,0 +1,27 @@
+//
+//  NSString+HJDC.m
+//  HJDC
+//
+//  Created by FuYunLei on 2018/5/29.
+//  Copyright © 2018年 FuYunLei. All rights reserved.
+//
+
+#import "NSString+HJDC.h"
+
+@implementation NSString (HJDC)
+
+
+- (NSString *)hjdc_cacheDir{
+    NSString *path = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES).firstObject;
+    return [path stringByAppendingPathComponent:self];
+}
+- (NSString *)hjdc_docDir{
+    NSString *path = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES).firstObject;
+    return [path stringByAppendingPathComponent:self];
+}
+- (NSString *)hjdc_tempDir{
+    NSString *path = NSTemporaryDirectory();
+    return [path stringByAppendingPathComponent:self];
+}
+
+@end
